417f85dac67d805ff2de678a1771b104a851386a
Ariadne Build <ariadne@local>
2026-08-27 14:38:15 +0530

    chore(prime-hermes): stop tracking build output (A1)

--- apps/prime-desktop/dist/electron-preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// electron/preload.ts
-var import_electron = require("electron");
-import_electron.contextBridge.exposeInMainWorld("primeHermes", {
-  gatewayBase: () => import_electron.ipcRenderer.invoke("prime:gateway-base"),
-  platform: () => import_electron.ipcRenderer.invoke("prime:platform"),
-  versions: () => import_electron.ipcRenderer.invoke("prime:versions")
-});
